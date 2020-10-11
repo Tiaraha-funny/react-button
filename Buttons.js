9442650cb5697ae30ba22eaf8edae2021740481a
Peta Jeannie <peta.jea@onja.org>
2020-10-10 22:53:51 -0600

    changing the code by using if statement

--- Buttons.js
+++ Buttons.js
@@ -2,11 +2,57 @@ import React from "react";
 import "./index.css";
 
 function Buttons(prop) {
+
+  let attributes;
+
+  if(prop.text === "<Button />") {
+    attributes = 'hover1';
+  }
+
+  if(prop.variant === "outline") {
+    attributes = 'hover2';
+  }
+  if(prop.variant === "text") {
+    attributes = 'hover3'
+  } 
+  if(prop.disabled === 'shadow') {
+    attributes = 'disabledShadow'
+  }
+  if(prop.disabled === 'disabled') {
+    attributes = 'disabled'
+  }
+  if(prop.icon === 'startIcon') {
+    attributes = 'iconBefore'
+  }
+  if(prop.icon === 'endIcon') {
+    attributes = "iconEnd"
+  }
+  if(prop.size === 'sm') {
+    attributes = "sm"
+  }
+  if(prop.size === 'md') {
+    attributes = 'md'
+  }
+  if(prop.size === 'lg') {
+    attributes = 'lg'
+  } 
+  if(prop.color === 'default'){
+    attributes = 'default'
+  }
+  if(prop.color === 'primary') {
+    attributes = 'primary'
+  }
+  if(prop.color === 'secondary') {
+    attributes = 'secondary'
+  }
+  if(prop.color === 'danger') {
+    attributes = 'danger'
+  }
   
   return (
-    <div>
+    <div className="btnDiv">
       <p>{prop.myBtns.text}</p>
-      <button className={prop.myBtns.name}>{prop.myBtns.type}</button>
+      <button className={attributes} disabled={prop.myBtns.disabled === false ? "" : "disabled"}>{prop.myBtns.type}</button>
     </div>
   );
 }
--- script.js
+++ script.js
@@ -4,133 +4,120 @@ import Buttons from "./Buttons";
 import "./index.css";
 
 class ButtonsApp extends React.Component {
-
   render() {
-    return <article>
-      <h1>Buttons</h1>
-      <div className="buttons">
-        <Buttons myBtns = {{
-            text: "<Button />",
-            type: "Default",
-            name: "btnOne",
-        }}/>
-        <Buttons myBtns = {{
-          text: "&:hover, &:focus",
-          type: "Default",
-          name: "btnTwo",
-        }}/>
-        <Buttons 
-        myBtns = {{
-          text: "<Button variant='outline'/>",
-          type: "Default",
-          name: "btnThree",
-        }}
+    return (
+      <article>
+        <h1>Buttons</h1>
+        <div className="buttons">
+          <Buttons
+            variant=""
+            text="<Button />"
+            type="Default"
+            name="hover1"
+            disabled=""
           />
-        <Buttons 
-        myBtns = {{
-          text: "&:hover, &:focus",
-          type: "Default",
-          name: "btnFour",
-        }}
-        />
-        <Buttons 
-        myBtns = {{
-          text: "<Button variant='text'/>",
-          type: "Default",
-          name: "btnFive",
-        }}/>
-        <Buttons 
-        myBtns = {{
-          text: "&:hover, &:focus",
-          type: "Default",
-          name: "btnSix",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button disableShadow />",
-          type: "Default",
-          name: "btnSeven",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button disabled/>",
-          type: "Disabled",
-          name: "btnEight",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button variant='text' disabled />",
-          type: "Disabled",
-          name: "btnNine",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button startIcon='local_grocery_store'/>",
-          type: "Default",
-          name: "btnTen",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button endIcon='local_grocery_store'/>",
-          type: "Default",
-          name: "btnEleven",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button size='sm' />",
-          type: "Default",
-          name: "btnTwelve",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button size='md' />",
-          type: "Default",
-          name: "btnThirteen",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button size='lg'/>",
-          type: "Default",
-          name: "btnFourteen",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button color='default' />",
-          type: "Default",
-          name: "btnFifteen",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button color='primary' />",
-          type: "Default",
-          name: "btnSixteen",
-        }}/>
-        <Buttons myBtns = {{
-          text: "<Button color='secondary' />",
-          type: "Secondary",
-          name: "btnSeventeen",
-        }}/>
-        
-        <Buttons myBtns = {{
-          text: "<Button color='danger' />",
-          type: "Danger",
-          name: "btnEighteen",
-        }}/>
 
-        <Buttons myBtns = {{
-          text: "&:hover, &:focus/>",
-          type: "Default",
-          name: "btnNineteen",
-        }}/>
+          <Buttons
+            variant="outline"
+            text="<Button variant='outline'/>"
+            type="Default"
+            name="hover2"
+            disabled=""
+          />
 
-        <Buttons myBtns = {{
-          text: " ",
-          type: "Default",
-          name: "btnTwenty",
-        }}/>
+          <Buttons
+            variant="text"
+            text="<Button variant='text'/>"
+            type="Default"
+            name="hover3"
+            disabled=""
+          />
 
-        <Buttons myBtns = {{
-          text: " ",
-          type: "Secondary",
-          name: "btnTwentyone",
-        }}/>
-        <Buttons myBtns = {{
-          text: " ",
-          type: "Danger",
-          name: "btnTwentytwo",
-        }}/>
-      </div>
-    </article>
+          <Buttons
+            disabled="shadow"
+            text="<Button disableShadow />"
+            type="Default"
+            name="disabledShadow"
+          />
+
+          <Buttons
+            text="<Button variant='text disabled'/>"
+            type="Disabled"
+            name="textDisabled"
+            disabled="disabled"
+          />
+
+          <Buttons
+            icon="startIcon"
+            text="<Button endIcon='local_grocery_store'/>"
+            type="Default"
+            name="iconBefore"
+            disabled=""
+          />
+          <Buttons
+            variant="text"
+            icon="endIcon"
+            text="<Button startIcon='local_grocery_store'/>"
+            type="Default"
+            name="endIcon"
+            disabled=""
+          />
+
+          <Buttons
+            text="<Button size='sm' />"
+            type="Default"
+            size="sm"
+            name="sm"
+            disabled=""
+          />
+
+          <Buttons
+            size="md"
+            text="<Button size='md' />"
+            type="Default"
+            name="md"
+            disabled=""
+          />
+
+          <Buttons
+            size="lg"
+            text="<Button size='lg'/>"
+            type="Default"
+            name="lg"
+            disabled=""
+          />
+          <Buttons
+            color="default"
+            text="<Button color='default' />"
+            type="Default"
+            name="defClr"
+            disabled=""
+          />
+          <Buttons
+            color="primary"
+            text="<Button color='primary' />"
+            type="Default"
+            name="primary"
+            disabled=""
+          />
+          <Buttons
+            color="secondary"
+            text="<Button color='secondary' />"
+            type="Secondary"
+            name="secondary"
+            disabled=""
+          />
+
+          <Buttons
+            color="danger"
+            text="<Button color='danger' />"
+            type="Danger"
+            name="danger"
+            disabled=""
+          />
+        </div>
+      </article>
+    );
   }
 }
 
